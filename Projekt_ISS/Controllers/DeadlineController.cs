ac735eafc18b181ae20f1e7867f785031fe455eb
deni2004 <112943691+deno2004@users.noreply.github.com>
2025-03-27 23:34:30 +0100

    conrollers

---
+++ Projekt_ISS/Controllers/DeadlineController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Projekt_ISS.Controllers
+{
+    public class DeadlineController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Projekt_ISS/Controllers/ProjectController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Projekt_ISS.Controllers
+{
+    public class ProjectController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
